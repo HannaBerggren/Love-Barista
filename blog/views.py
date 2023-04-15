e15873b2393e4d3fb2428162aa7832f0c9f1e5a2
HannaBerggren <hannajberggren@gmail.com>
2023-04-15 06:27:56 +0000

    Modifying code and add message tags

--- blog/views.py
+++ blog/views.py
@@ -7,8 +7,8 @@ from .forms import CommentForm
 
 class PostList(generic.ListView):
     model = Post
-    queryset = Post.objects.filter(status=1).order_by('-created_on')
-    template_name = 'index.html'
+    queryset = Post.objects.filter(status=1).order_by("-created_on")
+    template_name = "index.html"
     paginate_by = 6
 
 class PostDetail(View):
